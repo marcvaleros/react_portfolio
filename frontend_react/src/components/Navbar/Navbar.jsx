9d301cd69b66d25f33a98b2cbdd91083d03949fd
Marc Valeros <marcnathanielvaleros@gmail.com>
2023-01-25 23:13:08 +0800

    Update Navbar.jsx

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Navbar.scss'
+import { HiMenuAlt4, HiX} from 'react-icons/hi'
+import { motion } from 'framer-motion'
 import { images } from '../../constants'
+import { useState } from 'react'
 
 const navItems = ['home','about','work','skills','contact'];
 
 const Navbar = () => {
+  const [toggle, settoggle] = useState(false)
+
   return (
     <nav className='app__navbar'>
       <div className='app__navbar-logo'>
@@ -18,6 +23,17 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+
+      <div className='app__navbar-menu'>
+          <HiMenuAlt4 onClick={() => setToggle(true)}/>
+          { toggle && (
+              <motion.div>
+                whileInView= {{ x: [300,0]}}
+                transition={{duration: 0.85, ease: 'easeOut'}}
+              </motion.div>
+            )}
+
+      </div>
     </nav>
   )
 }
