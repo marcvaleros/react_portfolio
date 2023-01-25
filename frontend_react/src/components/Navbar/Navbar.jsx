aed0c8667637521d924f4abcb50edbc85e6bd6f8
Marc Valeros <marcnathanielvaleros@gmail.com>
2023-01-25 23:39:11 +0800

    fix navbar styling

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -27,16 +27,17 @@ const Navbar = () => {
           <HiMenuAlt4 onClick={() => setToggle(true)}/>
           { toggle && (
               <motion.div
-                whileInView= {{ x: [300,0]}}
+                whileInView= {{ x: [300,0] }}
                 transition={{duration: 0.85, ease: 'easeOut'}}
               >
                 <HiX onClick={ ()=> setToggle(false)}/>
-              
+                <ul>
                 { navItems.map((item)=> (
-                <li  key={item}>
-                  <a href={`#${item}`} onClick={ ()=> setToggle(false)}>{item}</a>
-                </li>
-              ))}
+                    <li  key={item}>
+                      <a href={`#${item}`} onClick={ ()=> setToggle(false)}>{item}</a>
+                    </li>
+                  ))}
+              </ul>
               </motion.div>
             )}
 
